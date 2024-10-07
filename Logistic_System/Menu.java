359b822a81d69c1b8eb0924eed036365d8376fb0
Aakash-M-o-d-i <work.aakash.modi@gmail.com>
2024-10-07 13:23:10 +0530

    jdbc successfully connected. Testing file only testing code run.

--- Logistic_System/Menu.java
+++ Logistic_System/Menu.java
@@ -1,8 +1,9 @@
 import java.security.SecureRandom;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Menu {
-    static public void menu(){
+    static public void menu() throws SQLException {
         String input = """
                 Select Option
                 1. Add New Package
@@ -51,7 +52,7 @@ public class Menu {
     private static void trackPackage() {
     }
 
-    private static void addNewPackage() {
+    private static void addNewPackage() throws SQLException {
         Scanner sc = new Scanner(System.in);
 
         // Sender Information
@@ -66,85 +67,118 @@ public class Menu {
         System.out.println("1.2. Email ID: ");
         String emailIdSen = sc.nextLine();
 
-        // Receiver Information
-        System.out.println("Enter Receiver Information:");
-        System.out.println("1. Receiver Name: ");
-        String nameRec = sc.nextLine();
-        System.out.println("2. Receiver Address");
-        String addrRec = sc.nextLine();
-        System.out.println("Receiver Contact Info:");
-        System.out.println("1.1. Phone Number: ");
-        String phoneNoRec = sc.nextLine();
-        System.out.println("1.2. Email ID: ");
-        String emailIdRec = sc.nextLine();
-
-        // Package Details
-        System.out.println("Enter Package Details: ");
-        System.out.println("1. Package Weight");
-        float packageWeight = sc.nextFloat();
-        sc.nextLine();  // Consume newline after float input
-        // Package dimensions input corrected as individual inputs (length, width, height)
-        System.out.println("2. Package Dimensions (Length, Width, Height)");
-        System.out.println("Length: ");
-        int length = sc.nextInt();
-        System.out.println("Width: ");
-        int width = sc.nextInt();
-        System.out.println("Height: ");
-        int height = sc.nextInt();
-        sc.nextLine();  // Consume newline after int inputs
-        System.out.println("3. Package Description (optional)");
-        String packageDesc = sc.nextLine();
-
-        // Shipping Details
-        System.out.println("Enter Shipping Details:");
-        System.out.println("1. Delivery Type (Standard/Express)");
-        String shippingType = sc.next();
-        sc.nextLine();  // Consume newline after next()
-        System.out.println("2. Origin Location");
-        String shippingOrgLocation = sc.nextLine();
-        System.out.println("3. Destination Location");
-        String shippingDesLocation = sc.nextLine();
-        System.out.println("4. Expected Delivery Date (DD/MM/YYYY)");
-        String shippingDate = sc.nextLine();  // For a date, store as a String for now
+//        // Receiver Information
+//        System.out.println("Enter Receiver Information:");
+//        System.out.println("1. Receiver Name: ");
+//        String nameRec = sc.nextLine();
+//        System.out.println("2. Receiver Address");
+//        String addrRec = sc.nextLine();
+//        System.out.println("Receiver Contact Info:");
+//        System.out.println("1.1. Phone Number: ");
+//        String phoneNoRec = sc.nextLine();
+//        System.out.println("1.2. Email ID: ");
+//        String emailIdRec = sc.nextLine();
+//
+//        // Package Details
+//        System.out.println("Enter Package Details: ");
+//        System.out.println("1. Package Weight");
+//        float packageWeight = sc.nextFloat();
+//        sc.nextLine();  // Consume newline after float input
+//        // Package dimensions input corrected as individual inputs (length, width, height)
+//        System.out.println("2. Package Dimensions (Length, Width, Height)");
+//        System.out.println("Length: ");
+//        int length = sc.nextInt();
+//        System.out.println("Width: ");
+//        int width = sc.nextInt();
+//        System.out.println("Height: ");
+//        int height = sc.nextInt();
+//        sc.nextLine();  // Consume newline after int inputs
+//        System.out.println("3. Package Description (optional)");
+//        String packageDesc = sc.nextLine();
+//
+//        // Shipping Details
+//        System.out.println("Enter Shipping Details:");
+//        System.out.println("1. Delivery Type (Standard/Express)");
+//        String shippingType = sc.next();
+//        sc.nextLine();  // Consume newline after next()
+//        System.out.println("2. Origin Location");
+//        String shippingOrgLocation = sc.nextLine();
+//        System.out.println("3. Destination Location");
+//        String shippingDesLocation = sc.nextLine();
+//        System.out.println("4. Expected Delivery Date (DD/MM/YYYY)");
+//        String shippingDate = sc.nextLine();  // For a date, store as a String for now
 
         sc.close();  // Close the Scanner when done
 
         // Generate Unique Package ID
         String uniqueID = generateSecureId();
 
-        // Store Package Data in Database. Pending!!
+        // Store Package Data in Database.
+
+        String sql = "INSERT INTO Sender (Sender_Name, Sender_Address, Sender_Phone_No,Sender_Email_Id)\n" +
+                "VALUES (?,?,?,?);";
+
+        String url = "jdbc:mysql://localhost:3306/logisticSys";
+        String userName = "root";
+        String password = "1111";
+
+        Connection con = DriverManager.getConnection(url,userName,password);
+
+        PreparedStatement preSt = con.prepareStatement(sql);
+
+        preSt.setString(1,nameSen);
+        preSt.setString(2,addrSen);
+        preSt.setString(3,phoneNoSen);
+        preSt.setString(4,emailIdSen);
+
+        int rowsInsert = preSt.executeUpdate();
+        if(rowsInsert > 0){
+            String sql1 = "select *from Sender";
+            ResultSet rs = preSt.executeQuery(sql1); // THis will fetch the result from databases. // for executing query's.
+            rs.next(); // why this? , bcz pointer are pointing to name column not exact name.
+            while (rs.next()) {
+                String nameSend = rs.getString("Sender_Name");  // Fetch column value
+                String addrSend = rs.getString("Sender_Address");  // Fetch  column value
+                String phoneNoSend = rs.getString("Sender_Phone_No");  // Fetch column value
+                String emailIdSend = rs.getString("Sender_Email_Id");  // Fetch column value
+
+                System.out.println("Name: " + nameSend + ", Address: " + addrSend + ", Phone_NO: " + phoneNoSend +" Email-Id: " + emailIdSend);
+            }
+        }
+
+
             // display store package data
-        System.out.printf("""
-                # Sender Information:
-                1. Sender Name: %s
-                2. Sender Address: %s
-                3. Sender Contact Info:\s
-                              3.1 Phone Number: %s
-                              3.2 Email ID: %s
-                """,nameSen,addrSen,phoneNoSen,emailIdSen);
-        System.out.printf("""
-                # Receiver Information:
-                1. Receiver Name: %s
-                2. Receiver Address: %s
-                3. Receiver Contact Info:\s
-                              3.1 Phone Number: %s
-                              3.2 Email ID: %s
-                """,nameRec,addrRec,phoneNoRec,emailIdRec);
-
-        System.out.printf("""
-                # Package Details:
-                1. Package Weight: %f
-                2. Package Dimensions (Length, Width, Height): %d, %d, %d
-                3. Package Description (optional): %s
-                """,packageWeight,length,width,height,packageDesc);
-
-        System.out.printf("""
-                # Shipping Details:
-                1. Delivery Type (Standard/Express): %s
-                2. Origin Location: %s
-                3. Destination Location: %s
-                4. Expected Delivery Date: %s
-                """,shippingType,shippingOrgLocation,shippingDesLocation,shippingDate);
+//        System.out.printf("""
+//                # Sender Information:
+//                1. Sender Name: %s
+//                2. Sender Address: %s
+//                3. Sender Contact Info:\s
+//                              3.1 Phone Number: %s
+//                              3.2 Email ID: %s
+//                """,nameSen,addrSen,phoneNoSen,emailIdSen);
+//        System.out.printf("""
+//                # Receiver Information:
+//                1. Receiver Name: %s
+//                2. Receiver Address: %s
+//                3. Receiver Contact Info:\s
+//                              3.1 Phone Number: %s
+//                              3.2 Email ID: %s
+//                """,nameRec,addrRec,phoneNoRec,emailIdRec);
+//
+//        System.out.printf("""
+//                # Package Details:
+//                1. Package Weight: %f
+//                2. Package Dimensions (Length, Width, Height): %d, %d, %d
+//                3. Package Description (optional): %s
+//                """,packageWeight,length,width,height,packageDesc);
+//
+//        System.out.printf("""
+//                # Shipping Details:
+//                1. Delivery Type (Standard/Express): %s
+//                2. Origin Location: %s
+//                3. Destination Location: %s
+//                4. Expected Delivery Date: %s
+//                """,shippingType,shippingOrgLocation,shippingDesLocation,shippingDate);
 
         // Display Confirmation Message
         System.out.println("Package has been successfully added.");
--- Logistic_System/Testing.java
+++ Logistic_System/Testing.java
@@ -1,15 +1,55 @@
-//import java.security.SecureRandom;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Testing {
+
+    public static void main(String[] args) throws Exception {
+        String sql = "insert into Students (Name, Roll_No, Age) values(?,?,?)";
+
+        String url = "jdbc:mysql://localhost:3306/testing";
+        String userName = "root";
+        String password = "1111";
+
+        Connection con = DriverManager.getConnection(url,userName,password);
+
+        PreparedStatement preSt = con.prepareStatement(sql);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = sc.nextLine();
+        System.out.println("Enter Roll_no");
+        int rollNo = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter age");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        preSt.setString(1,name);
+        preSt.setInt(2,rollNo);
+        preSt.setInt(3,age);
+
+        int rowsInsert = preSt.executeUpdate();
+        if(rowsInsert > 0){
+            String sql1 = "select *from Students";
+             ResultSet rs = preSt.executeQuery(sql1); // THis will fetch the result from databases. // for executing query's.
+            rs.next(); // why this? , bcz pointer are pointing to name column not exact name.
+            while (rs.next()) {
+                String name1 = rs.getString("name");  // Fetch 'name' column value
+                int rollNo1 = rs.getInt("roll_no");  // Fetch 'roll_no' column value
+                int age1 = rs.getInt("age");  // Fetch 'age' column value
+
+                System.out.println("Name: " + name1 + ", Roll No: " + rollNo1 + ", Age: " + age1);
+            }
+        }
+
+//        ResultSet rs = preSt.executeQuery(sql); // THis will fetch the result from databases. // for executing query's.
+//        rs.next(); // why this? , bcz pointer are pointing to name column not exact name.
 //
-//public class Testing {
-//    private static SecureRandom random = new SecureRandom();
+//        String name = rs.getString(1);
+//        System.out.println(name);
+//        System.out.printf("Connect successful!");
+        con.close();
+    }
+//    static public void db(){
 //
-//    public static String generateSecureId() {
-//        return new java.math.BigInteger(40, random).toString(32);  // Secure random ID
 //    }
-//    public static void main(String[] args) {
-//        String uniqueID = generateSecureId();
-//        System.out.println("Generated Secure Unique ID: " + uniqueID);
-//    }
-//}
-//
-//// nlflhds1gd207m4re96co1vmf9
+}
